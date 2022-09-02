dc37afc97230c65e8797d04746ce5f787f6f70f0
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-09-02 11:25:05 +0100

    player class __init__

--- main.py
+++ main.py
@@ -4,11 +4,28 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+# Defines the game screen size and setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+# Creates a new object from the Player class
+player = Player()
+car_manager = CarManager()
+
+# Lets the screen listen oyut for a key - "up".
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+# A continues while loop to let the game run and run until "Game over"
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+
+# To exit game screen
+screen.exitonclick()
--- player.py
+++ player.py
@@ -1,7 +1,19 @@
+from turtle import Turtle;
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+# global variables to use with this class
 
-
-class Player:
-    pass
+class Player(Turtle):
+    # This creates a new class called Player that inherits everything from Turtle.
+    # Player is then given the following attributes. 
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+    
+    # Function to move Player by the varibale MOVE_DISTANCE.
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
